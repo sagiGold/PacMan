26afff551dc076689215a4eddcca8a3a4a3cc601
Sagi Goldfarb <sagi3164@gmail.com>
2021-12-15 02:41:32 +0200

    test

--- main.cpp
+++ main.cpp
@@ -2,14 +2,6 @@
 //#include "Board.h"
 
 void main() {
-
-	/*Board board;
-	board.initBoard("pacman_01.screen.txt");
-	cout << board.getNumOfGhosts() << endl;
-	cout << (board.getGhostsPos())[0].getY() << (board.getGhostsPos())[0].getX() << endl;
-	cout << (board.getGhostsPos())[1].getY() << (board.getGhostsPos())[1].getX() << endl;
-	board.printBoard(false);*/
-
 	Game_Logic game;
 	game.runGame();
 }
